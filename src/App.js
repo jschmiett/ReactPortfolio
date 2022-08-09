2c77e3df688dfdfd99c7775fd654dc26d4494f97
Jennifer Schmiett <bailliejm@gmail.com>
2022-08-09 15:10:06 -0500

    Getting all file connections working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+import HelloReact from './components/HelloReact';
 
 function App() {
     console.log("Hello World");
+    return <HelloReact />;
 }
 
 export default App;
