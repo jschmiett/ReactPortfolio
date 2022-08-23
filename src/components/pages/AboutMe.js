db212ed3af8a1c634bfacc92644b349a3f2be55d
Jennifer Schmiett <bailliejm@gmail.com>
2022-08-23 14:39:21 -0500

    Trying to add profile picture

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -3,8 +3,16 @@ import React from "react";
 export default function Footer() {
     return (
         <>
-            <h2>About Me</h2>
-            <p>My name is Jennifer. Blah blah blah</p>
+            <div>
+                <img src="./public/profilePicture.jpg" style={{ width: 300, height: 300 }}></img>
+            </div>
+            <div>
+                <h2>About Me</h2>
+            </div>
+            <div>
+                <p>Hello there! My name is Jennifer. Several years ago curiosity drove me to try learning to code and I quickly realized just how fun and fulfilling it is for me. I'm a research-driven, detail oriented problem solver who thrives in a collaborative team. When I was a kid I wanted to be an astrophysicist so that I could always be learning new things and am excited to be going into a constantly changing field where there's always new skills and technologies to master.</p>
+            </div>
+
         </>
 
     )
